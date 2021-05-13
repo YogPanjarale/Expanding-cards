212f3912d055c8b6934bd4f4f0df0fcdd195b711
Anshu-pandey-codes <anshu191001@gmail.com>
2021-05-13 21:51:36 +0530

    Addid Resret Functionality

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 panels = document.querySelectorAll('.panel');
 con = document.getElementsByClassName('container');
+btn = document.getElementById('btn');
 console.log(panels)
 
 panels.forEach((element)=>{
     element.addEventListener('click',()=>{
         removeactive();
         element.classList.add('active');
+       
     })
 })
 
-
+btn.addEventListener('click',()=>{
+    panels.forEach((ele)=>{
+        ele.classList.remove('active');
+        
+    })
+})
     function removeactive(){
         panels.forEach((element)=>{
             element.classList.remove('active');
